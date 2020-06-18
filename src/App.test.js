0abcac2657ad3aa730aff330ae17d5ea10d232dd
Alessandro Sebastiani <sebbalex@gmail.com>
2020-06-18 19:40:47 +0200

    test dom

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const buttonElement = getByText(/Change Vehicle0 name/i);
-  expect(buttonElement).toBeInTheDocument();
+describe('check basic rendered components', () => {
+  // https://testing-library.com/docs/react-testing-library/cheatsheet
+  test('renders Load default values button', () => {
+    const { getByText } = render(<App />);
+    const buttonElement = getByText(/Load default values/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+  test('renders Change Vehicle0 name button', () => {
+    const { getByText } = render(<App />);
+    const buttonElement = getByText(/Change Vehicle0 name/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
