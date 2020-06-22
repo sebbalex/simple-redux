43e37051b44e48ee9ffe2e194343d0325012e12f
Alessandro Sebastiani <sebbalex@gmail.com>
2020-06-22 13:23:57 +0200

    remove unused import

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { composeWithDevTools } from "redux-devtools-extension";
-import { createStore, combineReducers } from "redux";
+import { createStore } from "redux";
 import rootReducer from "./reducers";
 
 const composedEnhancers = composeWithDevTools();
