7e6016c93ad05564d767780267d0c000886cd154
Alessandro Sebastiani <sebbalex@gmail.com>
2020-06-25 19:07:04 +0200

    tests: some reducer tests

--- src/store/__tests__/reducers.test.js
+++ src/store/__tests__/reducers.test.js
@@ -1,13 +1,30 @@
 import {vehicleById, initValue} from '../reducers/vehicleReducer';
-import { loadInitialValuesAction } from '../actions/actions';
+import {loadInitialValuesAction, realAddEventAction} from '../actions/actions';
 
 describe('vehicles reducer', () => {
   it('should return the initial state', () => {
-    expect(vehicleById(undefined, {})).toEqual({})
-  });
-  
-  it('should return the default values', () => {
-    expect(vehicleById(undefined, loadInitialValuesAction)).toEqual(initValue)
+    expect(vehicleById(undefined, {})).toEqual({});
   });
 
+  it('should return the default values', () => {
+    expect(vehicleById(undefined, loadInitialValuesAction)).toEqual(initValue);
+  });
+});
+
+describe('events reducer', () => {
+  it('should fails to add an event to a not exist vehicle', () => {
+    const eventId = 'event-TEST01',
+      eventTitle = 'eventTitleTest',
+      vehicleId = 'vehichleTest01';
+    expect(
+      vehicleById(undefined, {
+        type: realAddEventAction.type,
+        payload: {
+          eventId,
+          eventTitle,
+          vehicleId,
+        },
+      }),
+    ).toEqual({});
+  });
 });
